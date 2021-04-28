b43f8e202e57943c455ffe1667ee2f5dcbf7fd0b
Guilherme Soares <guilhermesoares1996@hotmail.com>
2021-04-28 19:57:00 -0300

    Criação do projeto da WebAPI

---
+++ SearchCustomers.WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchCustomers.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        
+    }
+}
