d3f26c9f1a5f87111e12c2e88398ac2307331b8e
Guilherme Soares <guilhermesoares1996@hotmail.com>
2021-05-01 15:52:53 -0300

    Estrutura de Repository, Datacontext e mapeamentos

---
+++ SearchCustomers.Domain/UserSys.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SearchCustomers.Domain
+{
+    public class UserSys
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserRoleId { get; set; }
+        public UserRole UserRole { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
